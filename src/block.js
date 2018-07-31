c29c9464b4f58a2581c675052866c3f36448f230
Andre Praeiro <andrepraeiro@gmail.com>
2018-07-31 00:29:13 -0400

    mining blockchain

--- src/block.js
+++ src/block.js
@@ -4,10 +4,20 @@ export default class Block {
         this.data = data
         this.previousHash = previousHash || '0'
         this.timeStamp = new Date()
+        this.nonce = 0
         this.hash = this.calculateHash()
     }
 
     calculateHash() {
-        return sha256(`${this.previousHash}${this.timeStamp}${this.data}`)
+        return sha256(`${this.previousHash}${this.timeStamp}${this.nonce}${this.data}`)
+    }
+
+    mineBlock(difficulty) {
+        const target = '0'.repeat(difficulty) //String.fromCharCode(difficulty)
+        while (this.hash.substring(0, difficulty) !== target) {
+            this.nonce++
+            this.hash = this.calculateHash()
+        }
+        console.log(`Block mined!!! : ${this.hash} `)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import Block from './block'
 const blockChain = []
 blockChain.push(new Block('Hi im the first block'))
+blockChain[0].mineBlock(3)
 blockChain.push(new Block('Yo im the second block', blockChain[blockChain.length - 1].hash))
+blockChain[1].mineBlock(3)
 blockChain.push(new Block('Hey im the third block', blockChain[blockChain.length - 1].hash))
+blockChain[2].mineBlock(3)
+
+const isChainValid = blockChain => {
+    let i = 0
+    blockChain.map((currentBlock, i) => {
+        let previousBlock = blockChain[i - 1]
+        if (currentBlock.hash !== currentBlock.calculateHash()) {
+            console.log('current hashes not equal')
+            return false
+        }
+        if (previousBlock && previousBlock.hash !== currentBlock.previousHash) {
+            console.log('previous hashes not equal')
+            return false
+        }
+    })
+    return true
+}
+
+if (isChainValid(blockChain) === true) {
+    console.log('hashes are equal')
+}
+
 console.log(blockChain)
